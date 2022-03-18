4975c4f90d4594e47bea7c46155a285a05e18dd0
christian julius guillen <juliusguillen@gmail.com>
2022-03-18 08:51:07 -0500

    commit tree cb71e221e1d892e471ff4c4a3b50d15711b76043

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function  writeCards(names, event) {
+    const newarray = []
+    for (let index = 0; index < names.length; index++) {
+        newarray.push(`Thank you, ${names[index]}, for the wonderful ${event} gift!`)
+    }
+    return newarray 
+
+}
+function countDown(count) {
+   while(count > -1 ) {
+       console.log(count);
+       count-- 
+   }
+}
